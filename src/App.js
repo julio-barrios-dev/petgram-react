701745e7c6409e4af0f2ec2c061912cf5241ae5f
Julio Barrios <julio.barrios.9618@gmail.com>
2022-05-28 20:46:19 -0300

    Rutas protegidas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { GlobalStyles } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
 import { NavBar } from './components/NavBar'
+import { User } from './pages/User'
+import { Favs } from './pages/Favs'
+import { NotRegisterUser } from './pages/NotRegisterUser'
 
 export const App = () => {
+  const isLogged = false
+
   return (
     <>
       <BrowserRouter>
@@ -16,6 +21,9 @@ export const App = () => {
               <Route path='/' element={<Home />} />
               <Route path='/pet/:id' element={<Home />} />
               <Route path='/detail/:detailId' element={<Detail/>} />
+              <Route path='/favs' element={isLogged ? <Favs /> : <Navigate to='/NotRegisterUser' /> } />
+              <Route path='/user' element={isLogged ? <User /> : <Navigate to='/NotRegisterUser' /> } />
+              <Route path='/NotRegisterUser' element={<NotRegisterUser />} />
         </Routes>
         <NavBar />
       </BrowserRouter>
---
+++ src/pages/NotRegisterUser.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NotRegisterUser = () => {
+  return (
+    <div>
+      <h1>NotRegisterUser</h1>
+    </div>
+  )
+}
